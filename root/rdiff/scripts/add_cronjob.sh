af59fb1556c8231a64be450016288ec594528577
DuaneAbrames <106115024+DuaneAbrames@users.noreply.github.com>
2025-05-21 12:42:25 -0400

    Update add_cronjob.sh
    Added option to use the file list.

--- root/rdiff/scripts/add_cronjob.sh
+++ root/rdiff/scripts/add_cronjob.sh
@@ -2,7 +2,7 @@
 
 CRONJOB=/cron/cronjob
 
-echo "${CRON_PATTERN} /usr/local/bin/rdiff-backup backup --compression /source/ /backups/" > $CRONJOB
+echo "${CRON_PATTERN} /usr/local/bin/rdiff-backup backup --compression --include-globbing-filelist /app/include-list.txt /source/ /backups/" > $CRONJOB
 # Suppression de cette ligne car rdifweb se charge de virer les anciens increments
 # C'est rdiffweb qui doit s'en charger
 #echo "${CRON_PATTERN} /usr/bin/rdiff-backup -v2 --force --remove-older-than "${REMOVE_OLDER_THAN:-1M}" /backup" >> $CRON_FOLDER/cronjob
